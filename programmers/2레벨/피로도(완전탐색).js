8a2fc3526b82538d07dd445a08767e8dbec9feac
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-27 14:46:17 +0900

    피로도

--- programmers/2레벨/피로도(완전탐색).js
+++ programmers/2레벨/피로도(완전탐색).js
@@ -29,3 +29,23 @@ console.log(
     [30, 10],
   ])
 );
+
+// function solution(k, dungeons) {
+//   let answer = 0;
+//   const visit = Array.from({ length: dungeons.length }, () => false);
+
+//   function dfs(rest, count) {
+//     answer = Math.max(answer, count);
+
+//     for (let i = 0; i < dungeons.length; i++) {
+//       const [need, use] = dungeons[i];
+//       if (!visit[i] && rest >= need) {
+//         visit[i] = true;
+//         dfs(rest - use, count + 1);
+//         visit[i] = false;
+//       }
+//     }
+//   }
+//   dfs(k, 0);
+//   return answer;
+// }
