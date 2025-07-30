000b3ccbf056e4db613a27c710f4a3fd5b7525cf
_wo_.ong <ckehfqkr302@gmail.com>
2025-07-30 17:22:29 +0900

    기지국 설치

---
+++ programmers/3레벨/기지국 설치.js
@@ -0,0 +1,16 @@
+function solution(n, stations, w) {
+  var answer = 0;
+
+  // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+  console.log('Hello Javascript');
+
+  return answer;
+}
+
+/**
+ * N개의 아파트가 일렬로 있다
+ * 이중 아파트 옥상에 4g 기지국이 설치된다
+ * 5g 수요가 높아져 4g -> 5g 변경
+ * 5gsms wjsekf qjadnlrk whqek
+ * 
+ */
